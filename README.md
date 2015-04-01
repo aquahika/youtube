5aa102eab1b470b6f7666b90955be2096881f9a9
Hikaru Sugiura <hikaru@sushitronics.com>
2015-04-01 12:03:36 +0000

    fix README.md

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # youtube
 
 ##
-`youtube = YouTube::new(:oauth_file => "motionium-oauth2.json")`
+`youtube = YouTube::new(:oauth_file => "motionium-oauth2.json")`  
 `youtube.upload(:file => 'video.mp4',:title=>'これはテスト動画です',:description=>'This is description'`
--- youtube.rb
+++ youtube.rb
@@ -96,5 +96,5 @@ end
 
 
 
-youtube = YouTube::new(:oauth_file => "motionium-oauth2.json")
-youtube.upload(:file => 'video.mp4',:title=>'これはテスト動画です',:description=>'This is description')
+#youtube = YouTube::new(:oauth_file => "motionium-oauth2.json")
+#youtube.upload(:file => 'video.mp4',:title=>'これはテスト動画です',:description=>'This is description')
